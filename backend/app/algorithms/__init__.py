59bf0856f577aecf768a1392c5f27b58805333bd
Damian@Vu <minhvdq3008@gmail.com>
2026-04-17 00:23:42 -0400

    feat: register EMAMACDStrategy, DonchianStrategy, VolumeBreakoutStrategy

--- backend/app/algorithms/__init__.py
+++ backend/app/algorithms/__init__.py
@@ -1,5 +1,11 @@
 from app.algorithms.rsi_divergence import RSIStrategy
+from app.algorithms.ema_macd import EMAMACDStrategy
+from app.algorithms.donchian_breakout import DonchianStrategy
+from app.algorithms.volume_breakout import VolumeBreakoutStrategy
 
 STRATEGIES: dict = {
-    "rsi_divergence": RSIStrategy,
+    "rsi_divergence":    RSIStrategy,
+    "ema_macd":          EMAMACDStrategy,
+    "donchian_breakout": DonchianStrategy,
+    "volume_breakout":   VolumeBreakoutStrategy,
 }
